dd0c2ca24a393ac1526463fe6a40eddbe6ab2494
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-04-15 00:15:07 +0800

    add gitignore

---
+++ spring-project-reactor-pracitce/src/main/java/com/lex/SpringProjectReactorPracitceApplication.java
@@ -0,0 +1,13 @@
+package com.lex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectReactorPracitceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectReactorPracitceApplication.class, args);
+    }
+
+}
