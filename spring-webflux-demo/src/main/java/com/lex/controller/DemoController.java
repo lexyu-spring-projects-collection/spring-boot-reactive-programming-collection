89e033ddfe825c2b8231c97ac4136cbb5f6e43dd
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-11-06 23:09:07 +0800

    Update DemoController.java

--- spring-webflux-demo/src/main/java/com/lex/controller/DemoController.java
+++ spring-webflux-demo/src/main/java/com/lex/controller/DemoController.java
@@ -5,15 +5,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 /**
  * @author : Lex Yu
  */
 @RestController
-@RequestMapping
 public class DemoController {
 
-	@GetMapping("/demo")
-	public Mono<String> greetingMessage(){
-		return getMessage();
-	}
+    @GetMapping("/demo")
+    public Mono<String> greetingMessage() {
+//        return computeMessage();
+
+        return computeMessage().zipWith(getNameFromDB())
+                .map(values -> {
+                    return values.getT1() + values.getT2();
+                });
+    }
+
+    private Mono<String> computeMessage() {
+        return Mono.just("Hello World").delayElement(Duration.ofSeconds(3));
+    }
+
+    private Mono<String> getNameFromDB() {
+        return Mono.just("LLLLLLLL").delayElement(Duration.ofSeconds(5));
+    }
 }
